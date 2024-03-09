ed223a808f472b842238d10c3707669cab5d5482
72123 <88994370+koonguy7838@users.noreply.github.com>
2024-03-09 20:36:06 +0700

    update-guy

--- pages/contact.php
+++ pages/contact.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="en">
 
 <head>
-    <title>Home</title>
+    <title>ติดต่อเรา</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
