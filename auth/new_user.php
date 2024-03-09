b11b7685e195fe3f2a278a4ca29f669fef1d071d
Solaris470 <ifnet470@gmail.com>
2024-03-09 15:03:21 +0700

    final finish
    ส่งแม่งทั้งอย่างงี้แหละ

--- auth/new_user.php
+++ auth/new_user.php
@@ -12,21 +12,29 @@ $hashed_password = password_hash($c_password, PASSWORD_DEFAULT);
 // $result = $conn->$checkQuery;
 // $row = $result->fetch_assoc();
 
-if ($row['count'] > 0) {
-    $error_message = "อีเมลหรือชื่อผู้ใช้ซ้ำกับในระบบ";
-    header('Location: regis.php?error=' . urlencode($error_message));
-    exit();
-} else {
+// if ($row['count'] > 0) {
+//     $error_message = "อีเมลหรือชื่อผู้ใช้ซ้ำกับในระบบ";
+//     header('Location: regis.php?error=' . urlencode($error_message));
+//     exit();
+// } else {
     // No duplicate found, proceed with the insertion
     $sql = "INSERT INTO users (c_name, c_tel, c_email,c_username, c_password) VALUES ('$c_name', '$c_tel', '$c_email', '$c_user', '$hashed_password')";
 
     if ($conn->query($sql)) {
-        header('Location: /CapMooiTle');
+        //Get User id
+        $query = "SELECT c_id, c_username,c_password FROM users WHERE c_username = '$c_user'";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        
+        $_SESSION['user_id'] = $row['c_id'];
+        $_SESSION['user_name'] = $row['c_username'];
+
+        header('Location: ../index.php');
     } else {
-        $error_message = "เกิดข้อผิดพลาดกรุณาติดต่อผู้ดูแลระบบ";
+        $error_message = "ชื่อผู้ใช้หรืออีเมลซ้ำ หากมีปัญหากรุณาติดต่อผู้ดูแลระบบ";
         header('Location: regis.php?error=' . urlencode($error_message));
         exit();
     }
-}
+// }
 $conn->close();
 ?>
